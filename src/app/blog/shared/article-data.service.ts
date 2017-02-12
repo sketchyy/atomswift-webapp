b57659d873e7b26e3927e0b6b2f35959f3c45627
sketchyy <sketchyy@ya.ru>
2017-02-12 11:20:52 +0300

    Update angular-cli. Add script for gh-pages deploy.

--- src/app/blog/shared/article-data.service.ts
+++ src/app/blog/shared/article-data.service.ts
@@ -9,7 +9,7 @@ export class ArticleDataService {
     this.articles = ['1', '2', '3', '4', '5'].map(id => {
       const article = new Article();
       article.id = id;
-      article.title = `title ${id}`;
+      article.title = `Title ${id}`;
       article.content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
         Maecenas vulputate enim sed tortor viverra malesuada. In sit amet nisi nec turpis consectetur luctus vitae id ex.
         Mauris posuere hendrerit diam at maximus. `;
